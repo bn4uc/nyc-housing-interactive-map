e9162ec578c7a0011c0643e796f150d3543f9ee2
bn4uc <noonanb@uchicago.edu>
2019-03-09 10:49:23 -0600

    fixing update cycle

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-console.log("H")
+console.log("Huuuu")
 
-//code based on http://bl.ocks.org/feyderm/e6cab5931755897c2eb377ccbf9fdf18
+//code altered from http://bl.ocks.org/feyderm/e6cab5931755897c2eb377ccbf9fdf18
 //and https://github.com/mcnuttandrew/capp-30239/tree/master/week-8-map/soln
 
 
-Promise.all([
-    fetch(
-      'data/nta-clean.json'
-    ).then(data => data.json()),
-    fetch(
-      'data/hmda_per_nta.json' //need to make this dependent on selection (or just address with update fill)
-     ).then(data => data.json()),
-  ]).then(combined => myVis(combined[0], combined[1])); 
+// Promise.all([
+//     fetch(
+//       'data/nta-clean.json'
+//     ).then(data => data.json()),
+//     fetch(
+//       'data/hmda_per_nta.json' //need to make this dependent on selection (or just address with update fill)
+//      ).then(data => data.json()),
+//   ]).then(combined => myVis(combined[0], combined[1])); 
 
-document.addEventListener('DOMContentLoaded', () => {
+
 Promise.all([
   'data/nta-clean.json', 
   'data/hmda_per_nta.json', 
@@ -45,11 +45,11 @@ Promise.all([
   ].map(url => fetch(url).then(data => data.json())))
     .then(data => myVis(data))
     .catch(function(error){
-      console.log("Error: ${error}");
+      console.log("Error");
     });
-  });
+
  
-//var [nta_clean, hmda_per_nta, hmda_nta_asian_2013] = data
+
 
 d3.csv("dropdown_options.csv").then(function(dataset) { //loading in the csv for the dropdown options
      console.log(dataset);
@@ -77,8 +77,30 @@ function computeDomain(data, key) {
 
 
 
-function myVis(ntaShapes, ntaValues) { //maybe this should be ntaShapes, and selectedData (but we must load it first)
-  console.log(ntaShapes, ntaValues);
+function myVis(data) { //maybe this should be ntaShapes, and selectedData (but we must load it first)
+  
+
+  var [ntaShapes, hmda_per_nta, 
+hmda_nta_general_2013, hmda_nta_general_2014, 
+hmda_nta_general_2015, hmda_nta_general_2016, hmda_nta_general_2017,
+
+hmda_nta_asian_2013, hmda_nta_asian_2014, 
+hmda_nta_asian_2015, hmda_nta_asian_2016, hmda_nta_asian_2017, 
+
+hmda_nta_black_2013, hmda_nta_black_2014, 
+hmda_nta_black_2015, hmda_nta_black_2016, hmda_nta_black_2017,
+
+hmda_nta_white_2013, hmda_nta_white_2014, 
+hmda_nta_white_2015, hmda_nta_white_2016, hmda_nta_white_2017,
+
+hmda_nta_other_2013, hmda_nta_other_2014, 
+hmda_nta_other_2015, hmda_nta_other_2016, hmda_nta_other_2017
+] = data;
+
+var selectedData = hmda_nta_general_2013; //give it something to start with (should this be a csv)
+
+  console.log(selectedData); //this works
+
   var div = d3.select("body") //trying to create a tooltip! 
     .append("div") //does not work
     .attr("class", "tooltip")       
@@ -100,12 +122,14 @@ function myVis(ntaShapes, ntaValues) { //maybe this should be ntaShapes, and sel
 //     ntaNameToVar[row.ntacode] = row.button_choice; //need this to be the chosen variable, from button
 //   }
 // console.log(ntaNameToVar);
-const varDomain = computeDomain(ntaValues, 'avg_inc'); //this needs to be made general variable responsive to buttons
-const incScale = d3.scaleLinear().domain([0, varDomain.max]).range([0, 1]);
-//const varScale = d3.scaleLinear().domain([0, varDomain.max]).range([0, 1]);
+const varDomain = computeDomain(selectedData, 'avg_inc');
+console.log(varDomain);
 
-const colorScale = d => d3.interpolateInferno(Math.sqrt(incScale(d))); //need to figure out how to make a legend for this
-//const colorScale = d => d3.interpolateInferno(Math.sqrt(varScale(d))); 
+const varScale = d3.scaleLinear().domain([0, varDomain.max]).range([0, 1]);
+
+
+const colorScale = d => d3.interpolateInferno(Math.sqrt(varScale(d))); //need to figure out how to make a legend for this
+
 
 var projection = d3.geoAlbers()
             .scale(70000)
@@ -128,6 +152,9 @@ const svg = d3.select('.vis-container')
       .text("Visualizing HMDA Data in New York City");
 
 
+
+console.log(selectedData);
+
   function updateFunction(selectedData) {
 
 console.log(svg);
@@ -137,41 +164,15 @@ const join = svg.selectAll('.ntacode')
   
 console.log(join, ntaShapes.features);
 
-// function updateFill(selection, selectedData) { //this is for choosing the variable, based on the buttons
 
-// const varDomain = computeDomain(ntaValues, 'avg_inc'); //this needs to be made general variable responsive to buttons
-
-
-
-//   varDomain = computeDomain(selectedData, selection); //is this how i would update this variable? 
-
-//   var d_extent = d3.extent(selection.data(), function(d) {
-//                 return parseFloat(d.properties[selectedData]);
-//             });
-
-//             rescaleFill(selection, d_extent);
-//         };
-
-    //const varDomain = computeDomain(selectedData, button_choice); //general version of above? (how to access button choice)
-
-    const ntaNameToInc = {}; //need to be general ntaNameToVar
-      for (let i = 0; i < ntaValues.length; i++) {
-        const row = ntaValues[i];
-        ntaNameToInc[row.ntacode] = row.avg_inc; //need to generalize this somehow so it fills with variable choice
+console.log(selectedData);
+const ntaNameToVar = {}; //need to be general ntaNameToVar
+  for (let i = 0; i < selectedData.length; i++){
+        const row = selectedData[i];
+        ntaNameToVar[row.ntacode] = row.avg_inc; //need to generalize this somehow so it fills with variable choice
       }
-    console.log(ntaNameToInc);
+    console.log(ntaNameToVar);
 
-// function rescaleFill(selection, d_extent) { //ne
-//     colorScale.domain(d_extent) //hmmm need to address this - this is giving a scale domain, but we cant just use compute domain
-//     selection.transition()
-//     .duration(700)
-//     .attr("fill", function(d) {
-//       var data = parseFloat(d.properties[selectedData]);
-//       return colorScale(data);
-//       });
-//     }
-
-   
 
     //THIS IS WORKING DO NOT TOUCH FIRST 6 lines  
     join.enter()
@@ -180,13 +181,13 @@ console.log(join, ntaShapes.features);
       .attr('stroke', 'black')
       .attr('d', d => geoGenerator(d))
       .merge(join)
-        .attr('fill', d => colorScale(ntaNameToInc[d.properties.ntacode]))
+        .attr('fill', d => colorScale(ntaNameToVar[d.properties.ntacode]))
         //.call(updateFill, selectedData) //throws error here, but also goes back to ln 49 and prints new chosen dropdown, then tries to run myvis
         .on("mouseover", function(d) {    //this does NOT WORK
           div.transition()    
             .duration(200)    
             .style("opacity", .9);    
-          div.html(d.ntacode)  //I think this needs to be a function that return the inc for each polygon
+          div.html(d.properties.ntaname)  //I think this needs to be a function that return the inc for each polygon
             .style("left", (d3.event.pageX) + "px")   
             .style("top", (d3.event.pageY - 28) + "px");  
         })          
@@ -196,19 +197,17 @@ console.log(join, ntaShapes.features);
           .style("opacity", 0); 
         });
   }
- var selectedData = "hmda_nta_general_2013.json"; //give it something to start with (should this be a csv)
-
+ 
   var dropDown = d3.select("#dropdown");
 
   dropDown.on("change", function() {
     checked = true;
-    selectedData = d3.event.target.value; //selectedData is the ".json" we need!! BUT NEED as input to myVis??
-    updateFunction(selectedData); //this is working to call myvis, but selected data is just text right now
-    //this seems to be working and getting hung up at 33, because selectedData is just text, need to get that to be json
+    selectedData = d3.event.target.value;
+    console.log(selectedData);
+    updateFunction(selectedData); 
   }); 
   // this is the first call to the update
-  updateFunction([]);
+  updateFunction(selectedData);
 }
-
 
 
