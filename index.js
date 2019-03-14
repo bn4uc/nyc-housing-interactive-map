3b95b34bc8470d3f79cd12d7376bbc46eddab8db
bn4uc <noonanb@uchicago.edu>
2019-03-14 18:20:23 -0500

    colors work for all vars

--- index.js
+++ index.js
@@ -128,10 +128,13 @@ var prettyVar = "Average Loan Amount";
     bottom: 10
   };
 
-function computeDomain(data, key) {
+function computeDomain(data, key) { //need to know what is going on in here
   return data.reduce((acc, row) => { 
+    if (!isFinite(row[key])) {
+      return acc;
+    }
     return {
-      min: Math.min(acc.min, row[key]),
+      min: Math.min(acc.min, row[key]), //something about this is not working for some vars/races/years
       max: Math.max(acc.max, row[key])
     };
   }, {min: Infinity, max: -Infinity});
@@ -157,74 +160,69 @@ const svg = d3.select('.vis-container')
       .style("text-anchor", "middle")
       .text("Visualizing HMDA Data in New York City");
 
-    // svg.append("text")
-    //   .attr("y", height/30 + 50)
-    //   .attr("x", width/5 -100)             
-    //   .style("text-anchor", "right")
-    //   .text("Neighborhood Tabulation Area (NTA): ");
+  svg.append("text")
+      .attr("y", height-100)
+      .attr("x", width - 400)             
+      .style("text-anchor", "middle")
+      .text("Source: Home Mortgage Discolure Act Data, CFPB");
 
 
-
-
-
-console.log(selectedData); //once changed this is just a name??? but when we have the equals above, it treats it as data
+console.log(selectedData); 
 
   function updateFunction(selectedData, selectedVar, prettyVar) {
 
-const varDomain = computeDomain(selectedData, selectedVar); //this works with selectedVar and changes the domain, but the ntaNametoVar still uses avg_inc
+const varDomain = computeDomain(selectedData, selectedVar); 
+console.log('11111', selectedVar)
 console.log(varDomain);
 
-const varScale = d3.scaleLinear().domain([0, varDomain.max]).range([0, 1]);
+const varScale = d3.scaleLinear().domain([varDomain.min, varDomain.max]).range([0, 1]);
+console.log(varScale);
 
 const colorScale = d => d3.interpolateYlOrRd(Math.sqrt(varScale(d)));
+console.log(colorScale);
 
-// COLOR LEGEND - code from https://d3-legend.susielu.com/#color-examples
 
-// var linear = d3.scaleLinear() //this is what they use on in .scale()
-//   .domain([0,varDomain.max])
-//   .range(["rgb(46, 73, 123)", "rgb(71, 187, 94)"]);
+  //color legend - won't show colors yet 
+//is this one better? with just one number? otherqise people might think the splits are weirder
+
+//   var svg2 = d3.select("svg");
+
+// svg2.append("g")
+//   .attr("class", "legendLinear")
+//   .attr("transform", "translate(110,85)");
+
+// var legend = d3.legendColor()
+//   .labelFormat(d3.format(".2f"))
+//   .useClass(true)
+//   .title("Color Legend") //how to get this to be the name of the button that is checked
+//   .titleWidth(100)
+//   .scale(varScale); //need to use something here that has colors and numbers - colorScale should work but doesnt have numbers i think 
+
+// svg2.select(".legendLinear")
+//   .call(legend);
+
+var quantize = d3.scaleQuantize()
+  .domain([ varDomain.min, varDomain.max])
+  .range(d3.range(5)
+    .map(function(i) { return "q" + i + "-9"; })); //what is this returning? the number splits? 
+
 
 var svg2 = d3.select("svg");
 
 svg2.append("g")
-  .attr("class", "legendLinear")
+  .attr("class", "legendQuant")
   .attr("transform", "translate(110,85)");
 
 var legend = d3.legendColor()
   .labelFormat(d3.format(".2f"))
   .useClass(true)
-  .title("Color Legend") //how to get this to be the name of the button that is checked
+  .title(prettyVar)
   .titleWidth(100)
-  .scale(varScale); //need to use something here that has colors and numbers - colorScale should work but doesnt have numbers i think 
+  .scale(quantize);
 
-svg2.select(".legendLinear")
+svg2.select(".legendQuant")
   .call(legend);
 
-  //END LEGEND 
-
-  //color legend with different color scale -- doesnt work to add color 
-
-// var quantize = d3.scaleQuantize()
-//   .domain([ 0, varDomain.max])
-//   .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
-
-// var svg2 = d3.select("svg");
-
-// svg2.append("g")
-//   .attr("class", "legendQuant")
-//   .attr("transform", "translate(20,20)");
-
-// var legend = d3.legendColor()
-//   .labelFormat(d3.format(".2f"))
-//   .useClass(true)
-//   .title("A title")
-//   .titleWidth(100)
-//   .scale(quantize);
-
-// svg2.select(".legendQuant")
-//   .call(legend);
-
-  ////end color legend attempt 
 
 console.log(svg);
 
@@ -233,11 +231,11 @@ const join = svg.selectAll('.ntacode')
   
 console.log(join, ntaShapes.features);
 
-console.log(selectedData); //this is updated, but just in name, not with the json!!!!
+console.log(selectedData); 
 const ntaNameToVar = {}; 
   for (let i = 0; i < selectedData.length; i++){
         const row = selectedData[i];
-        ntaNameToVar[row.ntacode] = row[selectedVar]; //need to generalize but this doesnt work as selectedVar!!!!
+        ntaNameToVar[row.ntacode] = row[selectedVar]; 
       }
     console.log(ntaNameToVar);
 
@@ -249,17 +247,14 @@ const ntaNameToVar = {};
       .attr('stroke', 'black')  //how to do like if na then fill = grey??
       .attr('d', d => geoGenerator(d))
       .merge(join)
-        .attr('fill', d => colorScale(ntaNameToVar[d.properties.ntacode])) //this doesnt change with selectedVar, thats why color gets screwed up
+        .attr('fill', d => colorScale(ntaNameToVar[d.properties.ntacode])) 
         .on("mouseover", function(d) { 
           div.transition()    
             .duration(200)    
             .style("opacity", .9);    
           div.text("Neighborhood Tabulation Area (NTA):  " + d.properties.ntaname + " ----"+ prettyVar +":  "+ ntaNameToVar[d.properties.ntacode]) //pretty var updates within function
-            .style("left", width/5 + 150 + "px")   
+            .style("left", width/5 + 210 + "px")   
             .style("top", height/30 + 50 + "px");
-         // div.text(ntaNameToVar[d.properties.ntacode]) //this is still avg income only
-         //   .style("left", width/5 + 140 + "px")   
-         //   .style("top", height/30 + 53 + "px");
         })          
         .on("mouseout", function(d) {   
           div.transition()    
@@ -273,18 +268,18 @@ const ntaNameToVar = {};
     checked = true;
     prettyVar = d3.event.target.value;
     console.log(prettyVar);
-    selectedVar = d3.event.target.id; //this is pulling the value, but then I need it to recognize as name of data
+    selectedVar = d3.event.target.id; 
     console.log(selectedVar, dataDict[selectedData], selectedData);
-    updateFunction(selectedData, selectedVar, prettyVar); //this works to recall, but need to use selectedVar above!!
+    updateFunction(selectedData, selectedVar, prettyVar); 
      }); 
 
   var dropDown = d3.select("#dropdown");
 
   dropDown.on("change", function() {
     checked = true;
-    selectedData = dataDict[d3.event.target.value]; //this is pulling the value, but then I need it to recognize as name of data
+    selectedData = dataDict[d3.event.target.value]; 
     console.log(selectedData);
-    updateFunction(selectedData, selectedVar, prettyVar); //maybe this is stored as a string and thus not triggering name of dataset
+    updateFunction(selectedData, selectedVar, prettyVar); 
   }); 
 
   // this is the first call to the update
